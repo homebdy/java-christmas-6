51c4b25a0dae9467cb488df0352104bdc761da82
김연진 <love_homebody@naver.com>
2023-12-15 15:03:04 +0900

    feat(OutputView): 이벤트 플래너 시작 메시지 출력

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,10 @@
+package christmas.view;
+
+import christmas.constant.OutputMessage;
+
+public class OutputView {
+
+    public void printStartService() {
+        System.out.println(OutputMessage.START_PLANNER.getMessage());
+    }
+}
