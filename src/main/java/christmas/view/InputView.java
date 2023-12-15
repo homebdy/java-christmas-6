5651c65d66115c350c5ab2de38e3e38499d496f7
김연진 <love_homebody@naver.com>
2023-12-15 15:15:09 +0900

    feat(InputVew): 주문 메뉴와 개수 입력

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,7 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 import christmas.domain.Day;
 import christmas.validator.InputValidator;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
+
+    private static final String SPLIT_REGEX = ",";
 
     private final InputValidator validator = new InputValidator();
 
@@ -12,5 +17,10 @@ public class InputView {
         String input = Console.readLine();
         validator.validateNumber(input);
         return new Day(Integer.parseInt(input));
+    }
+
+    public List<String> readMenu() {
+        String input = Console.readLine();
+        return Arrays.stream(input.split(SPLIT_REGEX)).toList();
     }
 }
