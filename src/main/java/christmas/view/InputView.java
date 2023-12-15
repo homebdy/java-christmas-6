0f58d56130d5c64432a65636553831a548b881d6
김연진 <love_homebody@naver.com>
2023-12-15 15:05:44 +0900

    feat(InputView): 예상 방문 날짜 입력

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,14 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import christmas.validator.InputValidator;
+
+public class InputView {
+
+    private final InputValidator validator = new InputValidator();
+
+    public String readDate() {
+        String input = Console.readLine();
+        return input;
+    }
+}
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -7,4 +7,8 @@ public class OutputView {
     public void printStartService() {
         System.out.println(OutputMessage.START_PLANNER.getMessage());
     }
+
+    public void printDate() {
+        System.out.println(OutputMessage.READ_DATE.getMessage());
+    }
 }
