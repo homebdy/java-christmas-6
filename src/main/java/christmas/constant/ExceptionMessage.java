93acd2fa038306d85e0fcf9dec61b172c7187a6a
김연진 <love_homebody@naver.com>
2023-12-15 15:22:29 +0900

    feat(InputValidator): 메뉴 형식이 예시와 다른 경우 예외 발생

--- src/main/java/christmas/constant/ExceptionMessage.java
+++ src/main/java/christmas/constant/ExceptionMessage.java
@@ -2,7 +2,8 @@ package christmas.constant;
 
 public enum ExceptionMessage {
 
-    NOT_INTEGER("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    NOT_INTEGER("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    INVALID_MENU("유효하지 않은 주문입니다. 다시 입력해 주세요.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
--- src/main/java/christmas/validator/InputValidator.java
+++ src/main/java/christmas/validator/InputValidator.java
@@ -6,10 +6,18 @@ import java.util.regex.Pattern;
 
 public class InputValidator {
     private static final String NUMBER_REGEXP = "^\\d*$";
+    private static final String DELIMITER_REGEXP = "^([a-zA-Zㄱ-힣]*-[\\d]*,)*([a-zA-Zㄱ-힣]*-[\\d]*)$";
 
     public void validateNumber(String input) {
         if (!Pattern.matches(NUMBER_REGEXP, input)) {
             ExceptionMessage message = ExceptionMessage.NOT_INTEGER;
+            throw new IllegalArgumentException(message.getMessage());
+        }
+    }
+
+    public void validateDelimiter(String input) {
+        if (!Pattern.matches(DELIMITER_REGEXP, input)) {
+            ExceptionMessage message = ExceptionMessage.INVALID_MENU;
             throw new IllegalArgumentException(message.getMessage());
         }
     }
